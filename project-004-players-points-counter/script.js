db6c2f80466f15e7e30e86ef09731903f8efad24
adrianojedaf <adrianojedaf@hotmail.com>
2026-03-03 21:54:38 +0100

    update js and styles

--- project-004-players-points-counter/script.js
+++ project-004-players-points-counter/script.js
@@ -1,51 +1,56 @@
 const container = document.querySelector('.container');
-const playersListContainer = document.querySelector('.container');
+const playersListContainer = document.querySelector('.players-list-container');
 const addPlayerButton = document.querySelector('.add-player-button');
-let playersList = [];
 
 let domController = {
-  createDomCard(name, points) {
+  createDomPlayerCard(name, points, id) {
     let cardContainer = document.createElement('div');
     let cardName = document.createElement('h2');
     let cardPoints = document.createElement('span');
-
     cardContainer.classList.add('player-card');
+    cardContainer.setAttribute("id", id);
     cardName.classList.add('name');
     cardPoints.classList.add('points');
-
     cardName.textContent = name;
     cardPoints.textContent = points;
-
     cardContainer.append(cardName, cardPoints);
-
     this.addPlayerToDom(cardContainer);
   },
 
   addPlayerToDom(playerElement) {
-    playersListContainer.prepend(playerElement);
+    playersListContainer.append(playerElement);
+  }
+};
+
+let dashboard = {
+  playersList: [],
+  nextId: 0,
+  createNewPlayer(name, points, id) {
+    let newPlayer = new Player(name, points, id);
+    this.nextId = this.nextId + 1;
+    this.playersList.unshift(newPlayer);
   }
 }
-
-let game = {
-  createNewPlayer(name, points) {
-    let newPlayer = new Player(name, points);
-    playersList.unshift(newPlayer);
-  }
-}
-
 class Player {
-  constructor(name, points) {
+  constructor(name, points, id) {
     this.name = name;
     this.points = points;
+    this.id = id;
     this.isActive = false;
   };
 }
 
-function clickHandler() {
+function handleCreatePlayer() {
   let playerName = document.querySelector('.add-player-form .name').value;
   let initialPoints = document.querySelector('.add-player-form .points').value;
-  game.createNewPlayer(playerName, initialPoints);
-  domController.createDomCard(playersList[0].name, playersList[0].points);
+  dashboard.createNewPlayer(playerName, initialPoints, dashboard.nextId);
+  domController.createDomPlayerCard(dashboard.playersList[0].name, dashboard.playersList[0].points, dashboard.playersList[0].id);
 }
 
-addPlayerButton.addEventListener('click', clickHandler);
+function handlePlayerCard(event) {
+  let actualCard = event.target.closest(".player-card");
+  console.log(dashboard.playersList);
+}
+
+addPlayerButton.addEventListener('click', handleCreatePlayer);
+playersListContainer.addEventListener('click', handlePlayerCard);
